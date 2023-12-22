fe2470f1e8c688d210685f7b469147dec77b35da
denis <denisfedotov2107@gmail.com>
2023-12-22 17:04:08 +0200

    propagation operator for manipulating arrays and objects

--- script.js
+++ script.js
@@ -476,11 +476,14 @@ function myFunc1() {
 myFunc1(); // Виклик функції
 */
 
+
+
+
 /*
 Домашнє завдання урок №10
 Напишіть функцію у JavaScript, яка містить одну локальну та одну глобальну змінні. 
 Демонструйте доступ до цих змінних всередині та поза функцією.
-*/
+
 
 let globalVar1 = 'Глобальна змінна'; // Глобальна змінна
 
@@ -491,3 +494,118 @@ function variables() {
 variables(); // Виклик функції
 
 console.log(globalVar1); // Виводимо значення глобальної змінної
+*/
+
+
+
+
+/*
+Урок №11
+Як використовувати оператор розповсюдження для маніпуляцій з масивами та об'єктами
+
+
+// Приклад 1: Копіювання масиву та додавання нового елементу
+let fruits = ['apple', 'banana', 'orange'];
+let fruitsCopy = [...fruits]; // Копіюємо масив fruits за допомогою оператора розповсюдження
+fruitsCopy.push('pear'); // Додаємо новий елемент 'pear' до fruitsCopy
+console.log(fruits); // Виводимо початковий масив fruits
+console.log(fruitsCopy); // Виводимо копію масиву fruits з доданим елементом 'pear'
+
+// Приклад 2: Копіювання об'єкту та зміна значення в копії
+let cars = [
+  {
+    model: "Lexus",
+    color: " red"
+  },
+  {
+    model: "Toyota",
+    color: "blue"
+  }
+]
+
+let carsCopy = [...cars]; // Копіюємо масив об'єктів cars за допомогою оператора розповсюдження
+carsCopy[0].color = 'black'; // Змінюємо значення в копії об'єкту
+console.log(cars); // Виводимо початковий масив об'єктів cars
+console.log(carsCopy); // Виводимо копію масиву об'єктів cars зі зміненим значенням
+
+// Приклад 3: Об'єднання двох масивів
+let colors = ['red', 'green', 'blue'];
+let shapes = ['circle', 'suare', 'triangle'];
+let colorsShapes = [...colors, ...shapes]; // Об'єднуємо два масиви за допомогою оператора розповсюдження
+console.log(colorsShapes); // Виводимо об'єднаний масив colorsShapes
+
+// Приклад 4: Створення нового об'єкту з використанням властивостей інших об'єктів
+let person1 = {
+  name: 'Alice',
+  age: 25
+}
+
+let person2 = {
+  name: 'Bob',
+  hobbies: ['gaming', 'coding']
+}
+
+let person3 = {
+  ...person1, // Копіюємо властивості об'єкту person1
+  ...person2 // Копіюємо властивості об'єкту person2
+}
+
+let person4 = {
+  ...person3, // Копіюємо властивості об'єкту person3
+  name: "Alice", // Змінюємо значення властивості name
+  hobbies: [...person3.hobbies, 'reading'] // Додаємо новий елемент до масиву hobbies
+};
+console.log(person4); // Виводимо новий об'єкт person4
+
+// Приклад 5: Додавання елементів до масиву
+let numbers = [1, 2, 3, 4, 5];
+let newNumbers1 = [0, ...numbers]; // Додаємо новий елемент 0 до початку масиву numbers
+let newNumbers2 = [...numbers, 6]; // Додаємо новий елемент 6 до кінця масиву numbers
+console.log(newNumbers1); // Виводимо новий масив newNumbers1
+console.log(newNumbers2); // Виводимо новий масив newNumbers2
+
+// Приклад 6: Додавання елементів до масиву в середині
+let numbers3 = [1, 2, 3, 4, 5,];
+let newNumbers3 = [...numbers3.slice(0, 2), 6, 7, ...numbers3.slice(2)]; // Додаємо нові елементи 6 та 7 після другого елемента масиву numbers3
+console.log(newNumbers3); // Виводимо новий масив newNumbers3
+
+// Приклад 7: Створення нового об'єкту з використанням властивостей іншого об'єкту та додавання нових властивостей
+let person5 = {
+  name: 'Alice',
+  age: 25
+}
+let newPerson = {
+  id: 1,
+  ...person5, // Копіюємо властивості об'єкту person5
+  hobbies: ['reading', 'writing'] // Додаємо новий масив hobbies
+};
+console.log(newPerson); // Виводимо новий об'єкт newPerson
+*/
+
+
+/*
+Домашнє завдання урок №11
+Напишіть функцію на JavaScript, яка використовує оператор розповсюдження для додавання 
+кількох нових елементів у середину існуючого масиву. Наприклад, функція повинна взяти масив [1, 2, 3, 4, 5], 
+додати елементи 6, 7 після другого елемента та повернути новий масив [1, 2, 6, 7, 3, 4, 5].
+*/
+
+function newDistributionOperator() {
+  // Створюємо початковий масив
+  let numbersMassif = [1, 2, 3, 4, 5];
+  
+  // Використовуючи оператор розповсюдження, додаємо елементи 6, 7 після другого елемента
+  let newNumbersMassif = [...numbersMassif.slice(0, 2), 6, 7, ...numbersMassif.slice(2)];
+  
+  // Повертаємо новий масив
+  return newNumbersMassif;
+}
+
+// Викликаємо функцію та виводимо результат
+console.log(newDistributionOperator());
+
+
+
+
+
+
